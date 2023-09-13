d05664ebf3145545d7b3b64a061fd454bef737ff
kazumaneet <mekbibtare@gmail.com>
2023-09-13 21:13:41 +0300

    Add a delete functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-library = [];
+var library = [];
 
 function Book(title, author, description, pages) {
   this.title = title;
@@ -7,7 +7,7 @@ function Book(title, author, description, pages) {
   this.pages = pages;
 }
 
-function addBook(book) {
+function addBookToLibrary(book) {
   library.push(book);
 }
 
@@ -17,28 +17,44 @@ function displayBooks(library, booksDisplay) {
     let book = document.createElement('li');
     book.textContent = library[i].title + ' ' + library[i].author;
     book.dataset.index = i;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.dataset.index = i;
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click',deleteBook);
+
+    book.appendChild(deleteButton);
     booksDisplay.appendChild(book);
   }
 }
 
-let book1 = new Book('Eternal Skies', 'Emily Johnson', 'In a world where the sky never changes, \
+function deleteBook(e){
+  var index = e.target.getAttribute('data-index');
+  var childBook = document.querySelector(`[data-index='${index}']`);
+  if (childBook.parentNode) {
+    childBook.parentNode.removeChild(childBook);
+  }
+
+}
+
+var book1 = new Book('Eternal Skies', 'Emily Johnson', 'In a world where the sky never changes, \
 four young adventurers set out on a perilous journey to uncover the secrets behind the immovable heavens.\
 As they navigate through ancient ruins and mystical forests, they must confront their fears and make choices \
 that will alter the fate of their world forever', 352);
-addBook(book1);
+addBookToLibrary(book1);
 
-let book2 = new Book('Whispers of the Night', 'Michael Williams', 'Set in 19th-century London, this gripping mystery follows\
+var book2 = new Book('Whispers of the Night', 'Michael Williams', 'Set in 19th-century London, this gripping mystery follows\
  detective William Thompson as he investigates a series of gruesome murders plaguing the city. As the body count rises and \
  the clues become more cryptic, Thompson must unravel the enigmatic whispers of the night to catch the elusive killer before\
   time runs out.', 428);
-addBook(book2);
+addBookToLibrary(book2);
 
-let book3 = new Book('The Quantum Gate', 'Sarah Parker', 'When brilliant physicist Dr. Catherine Evans discovers a hidden gateway\
+var book3 = new Book('The Quantum Gate', 'Sarah Parker', 'When brilliant physicist Dr. Catherine Evans discovers a hidden gateway\
  to alternate dimensions, she opens the door to a world of infinite possibilities and dangers. As governments and corporations vie\
   for control of this revolutionary technology, Catherine must navigate the treacherous waters of power and greed while struggling\
    with the moral implications of her discovery.', 512);
-addBook(book3);
+addBookToLibrary(book3);
 
-let booksDisplay = document.querySelector('#booksDisplay');
+var booksDisplay = document.querySelector('#booksDisplay');
 displayBooks(library, booksDisplay);
 
