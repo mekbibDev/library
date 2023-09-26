40901a650d2fd6c87e593c17843475e91a624289
kazumaneet <mekbibtare@gmail.com>
2023-09-26 14:57:32 +0300

    Refractor Code

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-var library = [];
+function Book(title, author, description, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.description = description;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.haveRead = function () {
+  this.read = !this.read;
+}
+
+function Library() {
+  this.books = [];
+}
+Library.prototype.addBook = function (book) {
+  this.books.push(book);
+}
+
+var library = new Library();
 var tbody = document.querySelector('tbody');
 var thead = document.querySelector('thead');
 var headerRow = document.createElement('tr');
@@ -7,6 +26,29 @@ var cancelButton = document.querySelector('#cancelButton');
 var readButton = document.querySelector('#read');
 var addBookDialog = document.querySelector('#addBookDialog');
 var addBookButton = document.querySelector('#addBookButton');
+
+var book1 = new Book('Eternal Skies', 'Emily Johnson', 'In a world where the sky never changes, \
+four young adventurers set out on a perilous journey to uncover the secrets behind the immovable heavens.\
+As they navigate through ancient ruins and mystical forests, they must confront their fears and make choices \
+that will alter the fate of their world forever', 352, true);
+library.addBook(book1);
+
+var book2 = new Book('Whispers of the Night', 'Michael Williams', 'Set in 19th-century London, this gripping mystery follows\
+ detective William Thompson as he investigates a series of gruesome murders plaguing the city. As the body count rises and \
+ the clues become more cryptic, Thompson must unravel the enigmatic whispers of the night to catch the elusive killer before\
+  time runs out.', 428, false);
+library.addBook(book2);
+
+var book3 = new Book('The Quantum Gate', 'Sarah Parker', 'When brilliant physicist Dr. Catherine Evans discovers a hidden gateway\
+ to alternate dimensions, she opens the door to a world of infinite possibilities and dangers. As governments and corporations vie\
+  for control of this revolutionary technology, Catherine must navigate the treacherous waters of power and greed while struggling\
+   with the moral implications of her discovery.', 512, true);
+library.addBook(book3);
+
+
+createTableHeader();
+displayBooks(library, tbody);
+
 
 showDialogButton.addEventListener('click', () => {
   addBookDialog.showModal();
@@ -20,50 +62,13 @@ readButton.addEventListener('click', (e) => {
 })
 addBookButton.addEventListener('click', addBookFromUser)
 
-createTableHeader();
-
-var book1 = new Book('Eternal Skies', 'Emily Johnson', 'In a world where the sky never changes, \
-four young adventurers set out on a perilous journey to uncover the secrets behind the immovable heavens.\
-As they navigate through ancient ruins and mystical forests, they must confront their fears and make choices \
-that will alter the fate of their world forever', 352, true);
-addBookToLibrary(book1);
-
-var book2 = new Book('Whispers of the Night', 'Michael Williams', 'Set in 19th-century London, this gripping mystery follows\
- detective William Thompson as he investigates a series of gruesome murders plaguing the city. As the body count rises and \
- the clues become more cryptic, Thompson must unravel the enigmatic whispers of the night to catch the elusive killer before\
-  time runs out.', 428, false);
-addBookToLibrary(book2);
-
-var book3 = new Book('The Quantum Gate', 'Sarah Parker', 'When brilliant physicist Dr. Catherine Evans discovers a hidden gateway\
- to alternate dimensions, she opens the door to a world of infinite possibilities and dangers. As governments and corporations vie\
-  for control of this revolutionary technology, Catherine must navigate the treacherous waters of power and greed while struggling\
-   with the moral implications of her discovery.', 512, true);
-addBookToLibrary(book3);
-
-
-
-displayBooks(library, tbody);
-function Book(title, author, description, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.description = description;
-  this.pages = pages;
-  this.read = read;
-}
-
-Book.prototype.haveRead = function () {
-  this.read = !this.read;
-}
-function addBookToLibrary(book) {
-  library.push(book);
-}
 
 function addBookFromUser(e) {
   var book = getFormData();
   if (validateForm(book) == false)
     return;
   e.preventDefault();
-  addBookToLibrary(book);
+  library.addBook(book);
   addBook(book);
   document.querySelector('form').reset();
   addBookDialog.close();
@@ -87,81 +92,76 @@ function validateForm(book) {
   }
   return true;
 }
-function displayBooks(library, booksDisplay) {
-  library.forEach(book => addBook(book));
+function displayBooks(library) {
+  library.books.forEach(book => addBook(book));
 }
 
 function addBook(book) {
-  let bookRow = document.createElement('tr');
+  var bookRow = document.createElement('tr');
   bookRow.dataset.title = book['title'];
 
-  let title = document.createElement('td');
-  title.textContent = book['title'];
-  bookRow.appendChild(title);
+  bookRow.appendChild(createCell('title',book));
+  bookRow.appendChild(createCell('author',book));
+  bookRow.appendChild(createCell('description',book));
+  bookRow.appendChild(createCell('pages',book));
 
-  let author = document.createElement('td');
-  author.textContent = book['author'];
-  bookRow.appendChild(author);
-
-  let description = document.createElement('td');
-  description.textContent = book['description'];
-  bookRow.appendChild(description);
-
-  let pages = document.createElement('td');
-  pages.textContent = book['pages'];
-  bookRow.appendChild(pages);
-
-  let read = document.createElement('td');
-  read.textContent = book.read ? 'Yes' : 'No';
-  read.dataset.titleRead = book.title + read.textContent;
-  read.appendChild(createReadToggleButton(read, book));
+  var read = document.createElement('td');
+  read.appendChild(createReadToggleButton(book));
   bookRow.appendChild(read);
 
-  let deleteTd = document.createElement('td');
-  let deleteButton = document.createElement('button');
-  deleteButton.textContent = 'delete';
-  deleteButton.dataset.title = book['title'];
-  deleteButton.addEventListener('click', deleteBook);
-  deleteTd.appendChild(deleteButton);
+  var deleteTd = document.createElement('td');
+  deleteTd.appendChild(createDeleteButton(book));
   bookRow.appendChild(deleteTd);
 
   tbody.appendChild(bookRow);
 }
+
+function createCell(name,book){
+  var cell = document.createElement('td');
+  cell.textContent = book[name];
+
+  return cell;
+}
+function createReadToggleButton(book) {
+  var readToggleButton = document.createElement('button');
+  readToggleButton.dataset.title = book['title'];
+  readToggleButton.dataset.titleRead = book.title;
+  readToggleButton.textContent = book.read ? 'Yes' : 'No';
+  readToggleButton.addEventListener('click', toggleRead)
+
+  return readToggleButton;
+}
+
+function createDeleteButton (book){
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = 'delete';
+  deleteButton.dataset.title = book['title'];
+  deleteButton.addEventListener('click', deleteBook);
+
+  return deleteButton;
+}  
+
 function deleteBook(e) {
   var title = e.target.getAttribute('data-title');
   var bookRow = document.querySelector(`tr[data-title='${title}']`);
   if (bookRow.parentNode) {
     bookRow.parentNode.removeChild(bookRow);
   }
-  library = library.filter(book => book.title != title);
+  library.books = library.books.filter(book => book.title != title);
 }
 
-function createReadToggleButton(element, book) {
-  let readToggleButton = document.createElement('button');
-  readToggleButton.dataset.title = book['title'];
-  readToggleButton.dataset.titleRead = book.title + element.textContent;
-  readToggleButton.textContent = 'read';
-  readToggleButton.addEventListener('click', toggleRead)
-
-  return readToggleButton;
-}
+ 
 function toggleRead(e) {
   var title = e.target.getAttribute('data-title');
-  var bookRow = document.querySelector(`tr[data-title='${title}']`);
-  var book = library.find(book => book.title === title);
+  var book = library.books.find(book => book.title === title);
   book.haveRead();
+  e.target.textContent = book.read ? 'Yes' : 'No';
 
-  var titleRead = e.target.getAttribute('data-title-Read');
-  var read = document.querySelector(`td[data-title-Read='${titleRead}']`);
-  read.textContent = book.read ? 'Yes' : 'No';
-  read.dataset.titleRead = book.title + read.textContent;
-  read.appendChild(createReadToggleButton(read, book));
 }
 
-
 function createTableHeader() {
-  var tableHeaader = ['title', 'author', 'description', 'pages', 'read', '']
-  tableHeaader.forEach(key => {
+  var tableHeader = ['title', 'author', 'description', 'pages', 'read', '']
+  tableHeader.forEach(key => {
     let header = document.createElement('th');
     header.textContent = key;
     header.scope = 'col';
